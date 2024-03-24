d621705f4a270ca3a0f3bd4e86235b314e1e37dc
Alp Kaan Aksu <hi@alpkaanaksu.com>
2024-03-24 15:57:59 +0100

    Add package-lock.json

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,8 +20,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'tellmimore', // Usually your GitHub org/user name.
+  projectName: 'tellmi', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
