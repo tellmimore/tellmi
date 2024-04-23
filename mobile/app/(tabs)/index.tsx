f46d6fa0dbd3ae90da2d9042fe9ae4948f0fd936
Luise Donat <donatluise@gmail.com>
2024-04-23 16:32:49 +0200

    Changed tabs into user questioning screens

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,13 +1,31 @@
-import { StyleSheet } from 'react-native';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Alert, Button, Pressable, StyleSheet, TextInput } from "react-native";
+
+import { useState } from "react";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>
+        Wie lange sind Sie schon mit Ihrem Partner zusammen?
+      </Text>
+
+      <TextInput placeholder="Anzahl der Jahre" keyboardType="numeric" />
+      <Text>Jahre und </Text>
+      <TextInput placeholder="Anzahl der Monate" keyboardType="numeric" />
+      <Text> Monate</Text>
+
+      <Pressable>
+        <Text>Continue</Text>
+      </Pressable>
+
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -16,16 +34,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
