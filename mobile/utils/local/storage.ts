72ad865f2b8d15e6140fb29af037e9fbf6b0b9df
Alp Kaan Aksu <hi@alpkaanaksu.com>
2024-05-02 19:14:11 +0200

    Export storage functions directly

--- mobile/utils/local/storage.ts
+++ mobile/utils/local/storage.ts
@@ -1,19 +1,13 @@
 import * as SecureStore from 'expo-secure-store';
 
-const saveValue = async (key: string, value: any) => {
+export const saveValue = async (key: string, value: any) => {
     await SecureStore.setItemAsync(key, value);
 }
 
-const getValue = async (key: string) => {
+export const getValue = async (key: string) => {
     return await SecureStore.getItemAsync(key);
 }
 
 const saveStudyInformation = (studyInformation: any) => {
     
-}
-
-export default {
-    saveValue,
-    getValue,
-    saveStudyInformation
 }
